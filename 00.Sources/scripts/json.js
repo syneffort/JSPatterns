93192eb53bd2b1ab9d718ad8a7d9daccd515808c
Pyunghwa Kim <peace_kim@lsmtron.com>
2021-04-30 12:11:21 +0900

    json handling

--- 00.Sources/scripts/json.js
+++ 00.Sources/scripts/json.js
@@ -8,3 +8,12 @@ var parsed = JSON.parse(string);
 
 console.log(string);
 console.log(parsed);
+
+var obj = { test: "yes"};
+var obj2 = obj;
+var obj3 = JSON.parse(JSON.stringify(obj)); // deep copy
+
+obj.test = "no";
+console.log("obj : " + obj.test);
+console.log("obj2 : " + obj2.test);
+console.log("obj3 : " + obj3.test);
